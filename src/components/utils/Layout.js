981359e0d6cae5b15f91a43579e6151925eee625
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-03-12 00:59:11 +0330

    Update layout

--- src/components/utils/Layout.js
+++ src/components/utils/Layout.js
@@ -4,7 +4,7 @@ import Footer from "../Footer";
 const Layout = ({ children }) => {
   return (
     <>
-      <section className="absolute left-[5rem] w-[90%] h-full overflow-auto">{children}</section>
+      <section className="absolute left-[5rem] w-[90%] h-full overflow-auto scrollbar-hide">{children}</section>
       <Menu />
       <Footer />
     </>
