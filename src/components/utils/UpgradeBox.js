f2df230f2770d28f1a14087048a0017396223e00
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-03-12 01:00:11 +0330

    Create upgrade box component

---
+++ src/components/utils/UpgradeBox.js
@@ -0,0 +1,20 @@
+const UpgradeBox = ({ name }) => {
+  return (
+    <>
+      <section className="flex flex-col w-[20rem] h-full rounded-lg bg-[#131722] p-4">
+        {/* box header */}
+        <section className="h-[1rem]">
+          <h5>
+            <span className="text-white">{name}</span> Analysis
+          </h5>
+        </section>
+        <section className="flex flex-col justify-center items-center h-full gap-3">
+          <p className="text-center">Upgrade Your Plan To See Lower Timeframes</p>
+          <button className="uppercase text-[#0690BD] rounded-3xl border border-[#0690BD] py-1 px-5">UPGRADE</button>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default UpgradeBox;
